40e16ac59ec581483ab4ea35cdf273d10be15978
devarsh281 <devarsh.doshi@technostacks.com>
2024-12-31 18:10:55 +0530

    first commit

---
+++ models/PostModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+const PostSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports=mongoose.model("Post",PostSchema)
